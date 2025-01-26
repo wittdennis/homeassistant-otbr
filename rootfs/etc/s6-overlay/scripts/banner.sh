0ca28e14cc8264c864043995ceb4ff2129e9e7c8
Dennis Witt <94747795+wittdennis@users.noreply.github.com>
2025-01-26 13:41:34 +0100

    fix: properly set banner script

--- rootfs/etc/s6-overlay/scripts/banner.sh
+++ rootfs/etc/s6-overlay/scripts/banner.sh
@@ -1,38 +1,29 @@
-# #!/usr/bin/with-contenv bash
-# # shellcheck shell=bash
-# # ==============================================================================
-# # Home Assistant Community Add-on: Base Images
-# # Displays a simple add-on banner on startup
-# # ==============================================================================
-# if bashio::supervisor.ping; then
-#     bashio::log.blue \
-#         '-----------------------------------------------------------'
-#     bashio::log.blue " Add-on: $(bashio::addon.name)"
-#     bashio::log.blue " $(bashio::addon.description)"
-#     bashio::log.blue \
-#         '-----------------------------------------------------------'
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+# ==============================================================================
+# Home Assistant Community Add-on: Base Images
+# Displays a simple add-on banner on startup
+# ==============================================================================
+echo \
+    '-----------------------------------------------------------'
+echo " Add-on: homeassistant-openthread-border-router"
+echo " Homeassistant version of OpenThread Border Router"
+echo \
+    '-----------------------------------------------------------'
 
-#     bashio::log.blue " Add-on version: $(bashio::addon.version)"
-#     if bashio::var.true "$(bashio::addon.update_available)"; then
-#         bashio::log.magenta ' There is an update available for this add-on!'
-#         bashio::log.magenta \
-#             " Latest add-on version: $(bashio::addon.version_latest)"
-#         bashio::log.magenta ' Please consider upgrading as soon as possible.'
-#     else
-#         bashio::log.green ' You are running the latest version of this add-on.'
-#     fi
+echo " Add-on version: null"
+echo ' You are running the latest version of this add-on.'
 
-#     bashio::log.blue " System: $(bashio::info.operating_system)" \
-#         " ($(bashio::info.arch) / $(bashio::info.machine))"
-#     bashio::log.blue " Home Assistant Core: $(bashio::info.homeassistant)"
-#     bashio::log.blue " Home Assistant Supervisor: $(bashio::info.supervisor)"
+echo " System: $(uname)" \
+    " ($(uname -m) / $(uname -n))"
+echo " Home Assistant Core: null"
+echo " Home Assistant Supervisor: null"
 
-#     bashio::log.blue \
-#         '-----------------------------------------------------------'
-#     bashio::log.blue \
-#         ' Please, share the above information when looking for help'
-#     bashio::log.blue \
-#         ' or support in, e.g., GitHub, forums or the Discord chat.'
-#     bashio::log.blue \
-#         '-----------------------------------------------------------'
-# fi
+echo \
+    '-----------------------------------------------------------'
+echo \
+    ' Please, share the above information when looking for help'
+echo \
+    ' or support in, e.g., GitHub, forums or the Discord chat.'
+echo \
+    '-----------------------------------------------------------'
