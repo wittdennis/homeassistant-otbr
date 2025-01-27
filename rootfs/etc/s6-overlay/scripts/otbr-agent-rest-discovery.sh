bf8ffb64d17d9cf3e2b24132fc26f482524f4429
Dennis Witt <94747795+wittdennis@users.noreply.github.com>
2025-01-27 07:50:23 +0100

    fix: rest agent discovery script disabled

--- rootfs/etc/s6-overlay/scripts/otbr-agent-rest-discovery.sh
+++ rootfs/etc/s6-overlay/scripts/otbr-agent-rest-discovery.sh
@@ -1,13 +1,13 @@
-# #!/usr/bin/with-contenv bash
-# # shellcheck shell=bash
-# # ==============================================================================
-# # Send OTBR discovery information to Home Assistant
-# # ==============================================================================
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+# ==============================================================================
+# Send OTBR discovery information to Home Assistant
+# ==============================================================================
 # declare config
 
 # config=$(
 #     bashio::var.json \
-#         host "$(bashio::addon.hostname)" \
+#         host "$(uname -n)" \
 #         port "^8081" \
 #         device "$(bashio::config 'device')" \
 #         firmware "$(ot-ctl rcp version | head -n 1)"
@@ -19,3 +19,5 @@
 # else
 #     bashio::log.error "Discovery message to Home Assistant failed!"
 # fi
+
+echo "Home Assistant automatic discovery disabled for this version of the addon"
