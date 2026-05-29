91a8da94abff50d1d375ec31d73700c550160483
Dennis Witt <dennis@derwitt.de>
2026-05-29 11:12:16 +0200

    feat!: promote Thread 1.4 beta to stable
    Upstream commit: https://github.com/home-assistant/addons/commit/4bed422596cc325c447af381506642a8344629c3

--- rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 # ==============================================================================
-# Select OTBR version and enable mDNSResponder for stable mode
+# Select OTBR version and handle automatic beta disable/promotion
 # ==============================================================================
 
+# Auto disable beta when we promote beta -> stable
+declare beta_auto_disable_version
+declare marker
+declare marker_version
+declare beta_active
+
+beta_auto_disable_version=1
+marker=/data/beta_auto_disabled
+marker_version=0
+if [[ -f ${marker} ]]; then
+    marker_version=$(< "${marker}")
+fi
+
+beta_active=false
 if [ "$BETA" == "1" ]; then
-    echo "INFO: Beta mode enabled, using OpenThread built-in mDNS."
+    if (( marker_version < beta_auto_disable_version )); then
+        echo "WARN: Disabling beta mode automatically; the previous beta is now stable."
+        export BETA=0
+    else
+        beta_active=true
+    fi
+fi
+echo "${beta_auto_disable_version}" > "${marker}"
+
+if [[ ${beta_active} == true ]]; then
+    echo "INFO: Beta mode enabled."
 
     ln -sf "/opt/otbr-beta/sbin/otbr-agent" /usr/sbin/otbr-agent
     ln -sf "/opt/otbr-beta/sbin/otbr-web" /usr/sbin/otbr-web
     ln -sf "/opt/otbr-beta/sbin/ot-ctl" /usr/sbin/ot-ctl
 else
-    echo "INFO: Stable mode, enabling mDNSResponder."
-
-    touch /etc/s6-overlay/s6-rc.d/user/contents.d/mdns
-    touch /etc/s6-overlay/s6-rc.d/otbr-agent/dependencies.d/mdns
+    echo "INFO: Stable enabled"
 
     ln -sf "/opt/otbr-stable/sbin/otbr-agent" /usr/sbin/otbr-agent
     ln -sf "/opt/otbr-stable/sbin/otbr-web" /usr/sbin/otbr-web
     ln -sf "/opt/otbr-stable/sbin/ot-ctl" /usr/sbin/ot-ctl
-    ln -sf "/opt/otbr-stable/sbin/mdnsd" /usr/sbin/mdnsd
 fi
 
 # ==============================================================================
